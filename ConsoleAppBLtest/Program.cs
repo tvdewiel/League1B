55e8ffa972769ec303b7f32ea6cf31c1d471db43
Tom Vande Wiele <tvdewiel@hotmail.com>
2022-03-25 11:55:23 +0100

    Add project files.

---
+++ ConsoleAppBLtest/Program.cs
@@ -0,0 +1,17 @@
+﻿using League.BL.Domein;
+using System;
+
+namespace ConsoleAppBLtest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Speler s = new Speler(10,"Jos",180,84);
+            Team t = new Team(13, "Beerschot");
+            s.ZetTeam(t);
+            Console.WriteLine("end");
+        }
+    }
+}
---
+++ League.BL/Domein/Speler.cs
@@ -0,0 +1,81 @@
+﻿using League.BL.Exceptions;
+using System;
+
+namespace League.BL.Domein
+{
+    public class Speler
+    {
+        public Speler(int id, string naam,  int? lengte, int? gewicht)
+        {
+            ZetId(id);
+            ZetNaam(naam);
+            //ZetRugnummer(rugnummer.Value);
+            if (lengte!=null) ZetLengte(lengte.Value);
+            if (gewicht.HasValue) ZetGewicht(gewicht.Value);
+        }
+
+        public int Id { get; private set; }
+        public string Naam { get; private set; }
+        public Team Team { get; private set; }
+        public int? Rugnummer { get; private set; }
+        public int? Lengte { get; private set; }
+        public int? Gewicht { get; private set; }
+
+        public void ZetNaam(string naam)
+        {
+            if (string.IsNullOrWhiteSpace(naam)) throw new SpelerException("ZetNaam");
+            Naam=naam.Trim();
+        }
+        public void ZetLengte(int lengte)
+        {
+            if (lengte<150)
+            {
+                //throw new SpelerException("ZetLengte");
+                SpelerException ex = new SpelerException("ZetLengte");
+                ex.Data.Add("lengte", lengte);
+                throw ex;
+            }
+            Lengte = Lengte;
+        }
+        public void ZetId(int id)
+        {
+            if (id <= 0) throw new SpelerException("ZetId");
+            Id = id;
+        }
+        public void ZetGewicht(int gewicht)
+        {
+            if (gewicht < 50) throw new SpelerException("Zetgewicht");
+            Gewicht=gewicht;
+        }
+        public void ZetRugnummer(int rugnummer)
+        {
+            if ((rugnummer <= 0) || (rugnummer > 99)) throw new SpelerException("ZetRugnummer");
+            Rugnummer = rugnummer;
+        }
+        public void VerwijderTeam()
+        {
+            Team = null;
+        }
+        public void ZetTeam(Team team)
+        {
+            if (team == null) throw new SpelerException("ZetTeam");
+            if (team == Team) throw new SpelerException("ZetTeam");
+            if (Team != null)
+            {
+                Team.VerwijderSpeler(this);
+            }
+            team.VoegSpelerToe(this);
+            Team = team;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Speler speler &&
+                   Id == speler.Id;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
